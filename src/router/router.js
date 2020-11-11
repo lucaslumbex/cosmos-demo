098349d88ed3386e1283f8bc17889a6e92d0b45d
lumbex <olumide.praise@qucoon.com>
2020-11-11 15:36:56 +0100

    fist commit in new face

--- src/router/router.js
+++ src/router/router.js
@@ -1,63 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Login from "../views/auth/Login";
-import Enrolment from "../views/auth/Enrolment";
+import Home from "../views/Home";
+// import Terms from "../views/Terms";
 import Dashboard from "../views/Dashboard";
-
-import CreateCompany from "../views/company/CreateCompany";
-import AddCompany from "../views/company/AddCompany";
-import AllAccountOfficers from "../views/officer/AllAccountOfficers";
-import OfficerMoreInfo from "../views/officer/OfficerMoreInfo";
-import AccountOfficerDetails from "../views/officer/AddAccountOfficerDetails";
-import CompanyDetails from "../views/company/CompanyDetails";
 
 Vue.use(VueRouter);
 
 const routes = [
+  // {
+  //   path: "/terms",
+  //   name: "terms",
+  //   component: Terms
+  // },
   {
-    path: "/company-details",
-    name: "company-details",
-    component: CompanyDetails
-  },
-  {
-    path: "/add-officer",
-    name: "add-officer",
-    component: AccountOfficerDetails
-  },
-  {
-    path: "/all-officers",
-    name: "all-officers",
-    component: AllAccountOfficers
-  },
-  {
-    path: "/officer-more-info",
-    name: "officer-more-info",
-    component: OfficerMoreInfo
+    path: "/",
+    name: "home",
+    component: Home
   },
   {
     path: "/dashboard",
     name: "dashboard",
     component: Dashboard
-  },
-  {
-    path: "/add-company",
-    name: "add-company",
-    component: AddCompany
-  },
-  {
-    path: "/create-company",
-    name: "create-company",
-    component: CreateCompany
-  },
-  {
-    path: "/enrolment",
-    name: "enrolment",
-    component: Enrolment
-  },
-  {
-    path: "/",
-    name: "login",
-    component: Login
   }
 ];
 
